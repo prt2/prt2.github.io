515c7a5548a53320051bc6a455bb60ba03c9803a
prt2 <prt2@sfu.ca>
2025-02-10 16:16:34 -0800

    Added Experience page styles

--- src/App.js
+++ src/App.js
@@ -66,6 +66,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/experience" element={<Experience />} />
       </Routes>
     </Router>
   );
